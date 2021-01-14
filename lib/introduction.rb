2fa2a4038e3bd7721f23b2d1e89bd6feaa1c5cc1
Jesse Brown <brownjesse8895@yahoo.com>
2021-01-14 01:43:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ end
 introduction_with_language("Dan", "Ember.js")
 
 def introduction_with_language_optional(name, language = "Ruby")
-puts "Hi, my name is #{name} and I am learning to progarm in #{langauge}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
